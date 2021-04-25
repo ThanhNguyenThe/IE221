4e10f7f3086f50bbf98a2f89283286aa2349bbb5
hoangtuan-hcmuit <tuanhoang.uit@gmail.com>
2021-04-25 20:12:01 +0700

    fix size mario, animation

--- objects/Mario.py
+++ objects/Mario.py
@@ -5,18 +5,21 @@ pygame.init()
 
 window = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Mario")
-walkRight = [pygame.image.load(os.path.join("img", "mario_run1.png")),
-            pygame.image.load(os.path.join("img", "mario_run2.png")),
-            pygame.image.load(os.path.join("img", "mario_run3.png"))]
 
-walkLeft = [pygame.transform.flip(pygame.image.load(os.path.join("img", "mario_run1.png")), True, False),
-            pygame.transform.flip(pygame.image.load(os.path.join("img", "mario_run2.png")), True, False),
-            pygame.transform.flip(pygame.image.load(os.path.join("img", "mario_run3.png")), True, False)]
+# run1 = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run1.png")), (48,48))
+# run2 = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run2.png")), (48,48))
+# run3 = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run2.png")), (48,48))
+walkRight = [pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run1.png")), (48,48)),
+            pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run2.png")), (48,48)),
+            pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run3.png")), (48,48))]
 
-deadImage = pygame.image.load(os.path.join("img", "mario_die.png"))
-jumpImage = pygame.image.load(os.path.join("img", "mario_jump.png"))
-char = pygame.image.load(os.path.join("img", "mario_stand.png"))
+walkLeft = [pygame.transform.flip(pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run1.png")), (48,48)), True, False),
+            pygame.transform.flip(pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run2.png")), (48,48)), True, False),
+            pygame.transform.flip(pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_run3.png")), (48,48)), True, False)]
 
+deadImage = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_die.png")), (48,48))
+jumpImage = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_jump.png")), (48,48))
+char = pygame.transform.scale(pygame.image.load(os.path.join("img", "mario_stand.png")), (48,48))
 clock = pygame.time.Clock()
 
 class Mario():
@@ -29,20 +32,29 @@ class Mario():
         self.isJump = False
         self.isLeft = False
         self.isRight = False
+        self.horizonFacing = 'right'
         self.walkCount = 0 
         self.jumpCount = 10
         
     def draw(self, window):
         if self.walkCount + 1 >= 9:
             self.walkCount =  0
-        if self.isLeft:
+        if self.isLeft and self.isJump == False:
             window.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
             self.walkCount += 1
-        elif self.isRight:
+        elif self.isRight and self.isJump == False:
             window.blit(walkRight[self.walkCount // 3], (self.x, self.y))
             self.walkCount += 1
+        elif self.isJump:
+            if self.horizonFacing == 'right':
+                window.blit(jumpImage, (self.x, self.y))
+            else:
+                window.blit(pygame.transform.flip(jumpImage, True, False), (self.x, self.y))
         else:
-            window.blit(char, (self.x, self.y))
+            if self.horizonFacing == 'right':
+                window.blit(char, (self.x, self.y))
+            else: 
+                window.blit(pygame.transform.flip(char, True, False), (self.x, self.y))
 
 def gameWindow():
     window.fill([255,255,255])
@@ -65,10 +77,12 @@ while run:
     if keys[pygame.K_LEFT] and player.x > player.vel:
         player.x -= player.vel
         player.isLeft = True
+        player.horizonFacing = 'left'
         player.isRight = False
     elif keys[pygame.K_RIGHT] and player.x < 500 - player.width - player.vel:
         player.x += player.vel
         player.isRight = True
+        player.horizonFacing = 'right'
         player.isLeft = False
     else:
         player.isRight = False
